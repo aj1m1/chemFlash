2899def1412a17a7ed2b65ad69f16ccbdb12aa9a
Jim Appiah <ajim@ad.unc.edu>
2022-03-18 20:05:17 -0400

    Updated app with new screen

--- KemFlash/ViewController.swift
+++ KemFlash/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BackTap: UILabel!
+    @IBOutlet weak var Frontap: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Frontap.isHidden = true
+    
     }
-//Chemistry
+//ChemistryFlash
 
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+    }
+    func updateFlashcard(question:String, answer:String) {
+        BackTap.text = answer
+        Frontap.text = question
+    
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        creationController.flashCardContoller = self
+    }
 }
 
